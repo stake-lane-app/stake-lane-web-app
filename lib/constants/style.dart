56bb6bf63047123fd0893cd0fad05eb2af6976c5
Lucas Alves <hello@luhalvesbr.dev>
2022-06-24 17:18:27 -0300

    fix(style): using black as active cold color (former color was blue)

--- lib/constants/style.dart
+++ lib/constants/style.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 Color light = const Color.fromARGB(128, 240, 240, 240);
 Color lightGrey = const Color.fromARGB(128, 87, 87, 87);
 Color dark = const Color.fromARGB(128, 47, 47, 47);
-Color activeColdColor = const Color.fromARGB(174, 1, 53, 184);
+Color activeColdColor = const Color.fromARGB(217, 0, 0, 0);
 Color activeHotColor = const Color.fromARGB(200, 194, 4, 4);
