5119bf31555733b77e77aa687fd79448c90e1b8c
Lucas Alves <hello@luhalvesbr.dev>
2022-06-26 23:24:09 -0300

    setting all needed logic to have running and finished fixture cards

--- lib/constants/match_status.dart
+++ lib/constants/match_status.dart
@@ -1,22 +1,55 @@
-final MatchStatus = [
-  {"code": "TBD", "description": "Time To Be Defined"},
-  {"code": "NS", "description": "Not Started"},
-  {"code": "1H", "description": "First Half"},
-  {"code": "HT", "description": "Halftime"},
-  {"code": "2H", "description": "Second Half"},
-  {"code": "ET", "description": "Extra Time"},
-  {"code": "P", "description": "Penalty In Progress"},
-  {"code": "FT", "description": "Match Finished"},
-  {"code": "AET", "description": "Match Finished After Extra Time"},
-  {"code": "PEN", "description": "Match Finished After Penalty"},
-  {"code": "BT", "description": "Break Time (in Extra Time)"},
-  {"code": "SUSP", "description": "Match Suspended"},
-  {"code": "INT", "description": "Match Interrupted"},
-  {"code": "PST", "description": "Match Postponed"},
-  {"code": "CANC", "description": "Match Cancelled"},
-  {"code": "ABD", "description": "Match Abandoned"},
-  {"code": "AWD", "description": "Technical Loss"},
-  {"code": "WO", "description": "Walkover"}
+Map toBeDefined = {"code": "TBD", "description": "Time To Be Defined"};
+Map notStarted = {"code": "NS", "description": "Not Started"};
+Map firstHalf = {"code": "1H", "description": "First Half"};
+Map halfTime = {"code": "HT", "description": "Halftime"};
+Map secondHalf = {"code": "2H", "description": "Second Half"};
+Map extraTime = {"code": "ET", "description": "Extra Time"};
+Map penalties = {"code": "P", "description": "Penalty In Progress"};
+Map finished = {"code": "FT", "description": "Match Finished"};
+Map extraTimeFinished = {
+  "code": "AET",
+  "description": "Match Finished After Extra Time"
+};
+Map penaltiesFinished = {
+  "code": "PEN",
+  "description": "Match Finished After Penalty"
+};
+Map breakTime = {"code": "BT", "description": "Break Time (in Extra Time)"};
+
+Map suspended = {"code": "SUSP", "description": "Match Suspended"};
+Map interrupted = {"code": "INT", "description": "Match Interrupted"};
+Map postponed = {"code": "PST", "description": "Match Postponed"};
+Map cancelled = {"code": "CANC", "description": "Match Cancelled"};
+Map abandoned = {"code": "ABD", "description": "Match Abandoned"};
+Map technialLoss = {"code": "AWD", "description": "Technical Loss"};
+Map walkover = {"code": "WO", "description": "Walkover"};
+
+List allowPredictionStatusCode = [
+  toBeDefined["code"],
+  notStarted["code"],
 ];
 
-List matchStatus = MatchStatus;
+List runningStatusCode = [
+  firstHalf["code"],
+  halfTime["code"],
+  secondHalf["code"],
+  extraTime["code"],
+  breakTime["code"],
+  penalties["code"],
+];
+
+List finishedStatusCode = [
+  finished["code"],
+  extraTimeFinished["code"],
+  penaltiesFinished["code"],
+  technialLoss["code"]
+];
+
+List specialEndingStatusCode = [
+  suspended["code"],
+  interrupted["code"],
+  postponed["code"],
+  cancelled["code"],
+  abandoned["code"],
+  walkover["code"],
+];
--- lib/pages/leagues/widgets/fixture_cards.dart
+++ lib/pages/leagues/widgets/fixture_cards.dart
@@ -2,6 +2,68 @@ import 'package:flutter/material.dart';
 import 'package:stake_lane_web_app/pages/leagues/widgets/cards/predictable.dart';
 import 'package:stake_lane_web_app/api/fixtures/my_fixtures.dart';
 import 'package:stake_lane_web_app/widgets/custom_text.dart';
+import 'package:stake_lane_web_app/constants/match_status.dart';
+
+Widget buildFixtureCard(fixture) {
+  int? homeTeamPrediction;
+  int? awayTeamPrediction;
+
+  if (fixture["prediction"] != null) {
+    homeTeamPrediction = fixture["prediction"]["home_team"];
+    awayTeamPrediction = fixture["prediction"]["away_team"];
+  }
+
+  String countryFlag = fixture["league"]["country_flag"] is String
+      ? fixture["league"]["country_flag"]
+      : '';
+
+  String leagueName = fixture["league"]["name"];
+  String isoDateStartingHour = fixture["starts_at_iso_date"];
+  String homeTeamName = fixture["home_team"]["name"];
+  String homeTeamLogo = fixture["home_team"]["logo"];
+  String awayTeamName = fixture["away_team"]["name"];
+  String awayTeamLogo = fixture["away_team"]["logo"];
+  int fixtureId = fixture["id"];
+  String statusCode = fixture["status_code"];
+
+  /* TODO: 
+    1st Solve CORS for images
+    https://stackoverflow.com/questions/65653801/flutter-web-cant-load-network-image-from-another-domain
+    https://cloud.google.com/storage/docs/configuring-cors
+
+    2nd create based on statusCode logic to call the cards builder
+    Depending on that we're gonna call Predictable, Running, Finished, etc.
+  */
+
+  if (allowPredictionStatusCode.contains(statusCode)) {
+    return PredictableCard(
+      countryFlag: countryFlag,
+      leagueName: leagueName,
+      isoDateStartingHour: isoDateStartingHour,
+      homeTeamName: homeTeamName,
+      homeTeamLogo: homeTeamLogo,
+      awayTeamName: awayTeamName,
+      awayTeamLogo: awayTeamLogo,
+      fixtureId: fixtureId,
+      homeTeamPrediction: homeTeamPrediction,
+      awayTeamPrediction: awayTeamPrediction,
+    );
+  }
+
+  if (runningStatusCode.contains(statusCode)) {
+    // TODO: Implement it
+  }
+
+  if (finishedStatusCode.contains(statusCode)) {
+    // TODO: Implement it
+  }
+
+  if (specialEndingStatusCode.contains(statusCode)) {
+    // TODO: Implement it
+  }
+  
+  return Container();
+}
 
 class FixtureCards extends StatelessWidget {
   int page;
@@ -29,35 +91,7 @@ class FixtureCards extends StatelessWidget {
           List<Widget> fixturesToScreen = [];
 
           for (var fixture in fixtures) {
-            int? homeTeamPrediction;
-            int? awayTeamPrediction;
-
-            if (fixture["prediction"] != null) {
-              homeTeamPrediction = fixture["prediction"]["home_team"];
-              awayTeamPrediction = fixture["prediction"]["away_team"];
-            }
-
-            // TODO: Solve CORS for images https://stackoverflow.com/questions/65653801/flutter-web-cant-load-network-image-from-another-domain
-            // TODO: https://cloud.google.com/storage/docs/configuring-cors
-
-            fixturesToScreen.add(
-              // TODO: create based on statusCode logic to call the cards builder
-              // TODO: Depending on that we're gonna call Predictable, Running, Finished, etc.
-              PredictableCard(
-                countryFlag: fixture["league"]["country_flag"] is String
-                    ? fixture["league"]["country_flag"]
-                    : '',
-                leagueName: fixture["league"]["name"],
-                isoDateStartingHour: fixture["starts_at_iso_date"],
-                homeTeamName: fixture["home_team"]["name"],
-                homeTeamLogo: fixture["home_team"]["logo"],
-                awayTeamName: fixture["away_team"]["name"],
-                awayTeamLogo: fixture["away_team"]["logo"],
-                fixtureId: fixture["id"],
-                homeTeamPrediction: homeTeamPrediction,
-                awayTeamPrediction: awayTeamPrediction,
-              ),
-            );
+            fixturesToScreen.add(buildFixtureCard(fixture));
           }
 
           return Column(
